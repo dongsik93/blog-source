fbded78c933b94102dd49541841e41cd363d3a86
MoonDongSik <ehdtlr9376@mailplug.co.kr>
2022-07-06 16:23:36 +0900

    [*] migration gradle kts

---
+++ dirty/app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id ("com.android.application")
+    id ("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+}
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "com.example.delayrequest"
+        minSdk = 26
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        val options = this
+        options.jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation (Dependency.KTX.CORE)
+    implementation (Dependency.AndroidX.APPCOMPAT)
+    implementation (Dependency.AndroidX.CONSTRAINT_LAYOUT)
+    implementation (Dependency.AndroidX.NAVIGATION_FRAGMENT)
+    implementation (Dependency.AndroidX.NAVIGATION_UI)
+    implementation (Dependency.Material.MATERIAL)
+    testImplementation (Dependency.TEST.JUNIT)
+    androidTestImplementation (Dependency.TEST.JUNIT_EXT)
+    androidTestImplementation (Dependency.TEST.ESPRESSO)
+}
---
+++ dirty/buildSrc/src/main/kotlin/Dependency.kt
@@ -0,0 +1,22 @@
+object Dependency  {
+    object KTX {
+        const val CORE = "androidx.core:core-ktx:1.7.0"
+    }
+
+    object AndroidX {
+        const val APPCOMPAT = "androidx.appcompat:appcompat:1.4.2"
+        const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.4"
+        const val NAVIGATION_FRAGMENT = "androidx.navigation:navigation-fragment-ktx:2.3.5"
+        const val NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:2.3.5"
+    }
+
+    object Material {
+        const val MATERIAL = "com.google.android.material:material:1.6.1"
+    }
+
+    object TEST {
+        const val JUNIT = "junit:junit:4.13.2"
+        const val JUNIT_EXT = "androidx.test.ext:junit:1.1.3"
+        const val ESPRESSO = "androidx.test.espresso:espresso-core:3.4.0"
+    }
+}
