1a5e705b3a877a75ee4ec439798334d83c1c6423
MoonDongSik <ehdtlr9376@mailplug.co.kr>
2022-07-11 16:03:15 +0900

    [*] usecase module 제거

--- dirty/presentation/src/main/java/com/example/presentation/usecase/GetAllContactUseCaseImpl.kt
+++
@@ -1,6 +0,0 @@
-package com.example.presentation.usecase
-
-import com.example.domain.model.Contact
-import com.example.domain.repository.ContactRepository
-import com.example.domain.usecase.GetAllContactUseCase
-import javax.inject.Inject
