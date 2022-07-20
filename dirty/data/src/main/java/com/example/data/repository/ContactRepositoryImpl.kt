dc6fe3eb4d4158574bcd568cfeb50f9e7c26fb71
MoonDongSik <ehdtlr9376@mailplug.co.kr>
2022-07-20 10:02:19 +0900

    [*] add todo list

--- dirty/data/src/main/java/com/example/data/repository/ContactRepositoryImpl.kt
+++ dirty/data/src/main/java/com/example/data/repository/ContactRepositoryImpl.kt
@@ -11,6 +11,13 @@ class ContactRepositoryImpl @Inject constructor(
     private val contactDao: ContactDao
 ) : ContactRepository {
     override fun saveContact(contact: Contact) {
+        /**
+         * TODO
+         * 1. 전처리 (요청된 연락처를 로컬 db에 저장
+         * 2. 서버로 upSync
+         * 2-1. 성공 처리 : dirty sync 해제 처리
+         * 2-2. 실패 / 오류 처리 : dirty sync 등록
+         */
         contactDao.insert(contact.toEntity())
     }
 
